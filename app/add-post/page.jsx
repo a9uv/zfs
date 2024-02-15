8fb79032b90bb496b2809175a8b512032316f390
regmi2 <regmi2@wisc.edu>
2024-02-15 09:47:47 -0600

    completed - delete post, fetch post, add post

---
+++ app/add-post/page.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from "react";  
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function AddPost() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const router = useRouter()
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleContentChange = (event) => {
+        setContent(event.target.value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const res = await fetch("/api/add-post", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            body: JSON.stringify({
+                title,
+                content,
+            }),
+            });
+            
+
+        if (res.status === 200) {
+            alert("Post created successfully!");
+            router.refresh()
+        } else {
+            alert("Post creation failed!");
+        }
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    return (
+        <main>
+            <Link href={'/'}> View Feed </Link>
+            <h1>Add Post</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="title" onChange={handleTitleChange} placeholder="Title" />
+                <textarea name="content" onChange={handleContentChange} eplaceholder="Content"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+    )
+}
